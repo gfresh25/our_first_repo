7a707eb9eec6b738319043da2f52bfbf6568c218
Garrett Freshwater <garrettfreshwater@gmail.com>
2018-02-07 09:16:17 -0500

    added a gets.chomp and method

--- our_first_repo.rb
+++ our_first_repo.rb
@@ -1,19 +1,17 @@
 # OUR FIRST REPO
 
 puts "Hello Universe"
+input = gets.chomp.to_i
 
-
-
-
-
-
-
-
-
-
-
-
-
-def why(num1,num2)
-  num1 + num2
+def add
+  if input == 10
+  puts "this is a new method"
+else
+  puts "sorry not today jr."
 end
+
+
+def add(num1,num2)
+  (num1 + num2).round
+end
+
